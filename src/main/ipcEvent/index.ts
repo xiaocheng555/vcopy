d371dc4545cbb45afca57cd1e235a3664878df9f
xiaocheng555 <1052745517@qq.com>
2022-10-29 11:21:35 +0800

    feat: 打包信息修改

--- src/main/ipcEvent/index.ts
+++ src/main/ipcEvent/index.ts
@@ -3,12 +3,14 @@ import createCopyWindow from '../windows/copyWindow'
 
 export default function initIpcEvent () {
   ipcMain.on('create-copy-window', (event, url: string) => {
+    console.log(event)
     createCopyWindow(url)
   })
   ipcMain.on('change-config:isCrack', (event, data: boolean) => {
+    console.log(event)
     global.Config.isCrack = data
   })
-  ipcMain.handle('get-config', (event) => {
+  ipcMain.handle('get-config', () => {
     return global.Config
   })
 }
